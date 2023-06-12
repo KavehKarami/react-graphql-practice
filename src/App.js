30b55a682aa33bab51aa79a544bdc171cdd7e194
KavehKarami <kaveh.karami.kk@gmail.com>
2023-06-12 14:32:44 +0330

    :tada: add search page to route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import "./App.css";
+import { Routes, Route } from "react-router";
 import Character from "./pages/Character";
 import CharacterList from "./pages/CharactersList";
-import { Routes, Route } from "react-router";
+import Search from "./pages/Search";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<CharacterList />} exact />
+        <Route path="/search" element={<Search />} exact />
         <Route path="/:id" element={<Character />} exact />
       </Routes>
     </div>
