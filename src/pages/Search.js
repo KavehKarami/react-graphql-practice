46ecaf9dbad9371ada8204c30ae65ec951b3f05d
KavehKarami <kaveh.karami.kk@gmail.com>
2023-06-12 14:32:07 +0330

    :tada: create search page with useLazyQuery

---
+++ src/pages/Search.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { gql, useLazyQuery } from "@apollo/client";
+
+const GET_CHARACTER_LOCATIONS = gql`
+  query GetCharacterLocations($name: String!) {
+    characters(filter: { name: $name }) {
+      results {
+        location {
+          name
+        }
+      }
+    }
+  }
+`;
+
+function Search() {
+  const [name, setName] = useState("");
+  const [getLocations, { data, loading, error }] = useLazyQuery(
+    GET_CHARACTER_LOCATIONS,
+    {
+      variables: { name },
+    }
+  );
+
+  return (
+    <div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={getLocations}>Search</button>
+
+      {loading && <div>loading...</div>}
+      {error && <div>Something went wrong</div>}
+      {data && (
+        <div>
+          <ul>
+            {data.characters.results.map((character) => (
+              <li>{character.location.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Search;
